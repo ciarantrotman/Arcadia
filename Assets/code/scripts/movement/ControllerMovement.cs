833942e0aecd5da53aff0c6ac793ec98d5d3e683
Ciaran Trotman <ciarantrotman@gmail.com>
2024-08-10 16:58:55 +0100

    Add Basic Twin Stick Controls

--- Assets/code/scripts/movement/ControllerMovement.cs
+++ Assets/code/scripts/movement/ControllerMovement.cs
@@ -1,15 +1,18 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
 namespace code.scripts.movement {
     public class ControllerMovement : MonoBehaviour {
         private PlayerControls controls;
 
         [SerializeField, Range(0f, 1f)] private float multiplier = 1f;
-
-        private static readonly float[] IsometricAngles = { 60, 120, -120, -60, 0, 90, -90, 180 };
+        
+        private static readonly float[] PixelAdjustedIsometricAngles = { 63.5f, 116.5f, -116.5f, -63.5f, 0, 90, -90, 180 };
+        
+        private static readonly int XValue = Animator.StringToHash("x_value");
+        private static readonly int YValue = Animator.StringToHash("y_value");
+        
+        [SerializeField] private Animator body;
+        [SerializeField] private Animator head;
         
         private Vector2 translation;
         private Vector2 rotation;
@@ -39,30 +42,36 @@ namespace code.scripts.movement {
             
             Vector3 framerate_independent_translation = (new Vector3(snapped_translation.x, snapped_translation.y) * Time.deltaTime);
             transform.Translate(framerate_independent_translation * multiplier, Space.Self);
+
+            Vector3 normalised_translation_vector = framerate_independent_translation.normalized;
+            
+            body.SetFloat(XValue, normalised_translation_vector.x);
+            body.SetFloat(YValue, normalised_translation_vector.y);
         }
 
         private void ApplyRotation() {
             snapped_rotation = snap_to_isometric_angles(rotation);
+            
+            Vector3 framerate_independent_rotation = (new Vector3(snapped_rotation.x, snapped_rotation.y) * Time.deltaTime);
+            Vector3 normalised_rotation_vector = framerate_independent_rotation.normalized;
+            
+            head.SetFloat(XValue, normalised_rotation_vector.x);
+            head.SetFloat(YValue, normalised_rotation_vector.y);
         }
         
         private static Vector2 snap_to_isometric_angles(Vector2 vector) {
             if (vector == Vector2.zero) return Vector2.zero;
-
-            // Calculate the angle of the input vector
+            
             float angle = Vector2.SignedAngle(Vector2.up, vector);
-
-            // Find the closest isometric angle
             float min_difference = float.MaxValue;
             float snapped_angle = 0;
 
-            foreach (float iso_angle in IsometricAngles)
+            foreach (float iso_angle in PixelAdjustedIsometricAngles)
             {
                 float difference = Mathf.Abs(angle - iso_angle);
-                if (difference < min_difference)
-                {
-                    min_difference = difference;
-                    snapped_angle = iso_angle;
-                }
+                if (!(difference < min_difference)) continue;
+                min_difference = difference;
+                snapped_angle = iso_angle;
             }
             
             Vector2 snapped_vector = Quaternion.Euler(0, 0, snapped_angle) * Vector2.up;
@@ -73,7 +82,6 @@ namespace code.scripts.movement {
             Vector3 position = transform.position;
             Debug.DrawRay(position, translation, Color.blue);
             Debug.DrawRay(position, snapped_translation, Color.blue);
-            
             
             Debug.DrawRay(position, rotation, Color.red);
             Debug.DrawRay(position, snapped_rotation, Color.red);
